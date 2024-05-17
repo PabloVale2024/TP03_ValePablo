c4591864d1426b02226809023eb9a90f1ab29199
PabloVale2024 <166053449+PabloVale2024@users.noreply.github.com>
2024-05-17 20:28:00 -0300

    tp3 con css

---
+++ src/main/java/ar/edu/unju/fi/cotroller/FormClienteController.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.cotroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class FormClienteController {
+	@GetMapping({"/Formulario"}) 
+	//indica la raiz
+	public String getFormCliente() {
+	
+	return "formCliente";
+	}
+	
+
+}
